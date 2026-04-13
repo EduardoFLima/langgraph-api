abf79f8a5b17ab04890bc45bee0d94e853e02d06
Eduardo Lima <eduefi@gmail.com>
2026-04-13 12:00:20 +0200

    configure open api client

--- src/adapters/outbound/model_clients/open_api_client.py
+++ src/adapters/outbound/model_clients/open_api_client.py
@@ -1,3 +1,7 @@
+from langchain.agents import create_agent
+from langchain.messages import HumanMessage, SystemMessage
+from langchain_openai import ChatOpenAI
+
 from src.config import Settings
 from src.domain.ports.model_client_port import ModelClientPort
 
@@ -6,6 +10,22 @@ class OpenAPIClient(ModelClientPort):
 
     def __init__(self, settings: Settings):
         self._settings = settings
+
+        self._client = ChatOpenAI(
+            api_key=self._settings.openrouter_api_key,
+            model=self._settings.models[0],
+            temperature=self._settings.temperature,
+            base_url="https://openrouter.ai/api/v1",
+            default_headers={
+                "HTTP-Referer": self._settings.http_referer,
+                "X-Title": self._settings.x_title,
+            },
+            extra_body={
+                "order":"arcee-ai",
+                "models": self._settings.models,
+                "provider": self._settings.provider,
+            },
+        )
 
     def send_prompt(self, prompt):
         print("this is the key:", self._settings.open_router_api_key)
--- src/config.py
+++ src/config.py
@@ -4,8 +4,20 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    open_router_api_key: str
+    openrouter_api_key: str
 
+    models: list[str] = [
+        "meta-llama/llama-3.1-8b-instruct",
+        "openai/gpt-oss-20b",
+        "nvidia/nemotron-3-super-120b-a12b:free"
+        ]
+
+    http_referer: str = "some.web.site"
+    x_title: str = "Testing chat agent"
+    provider: dict = dict(sort=dict(by="price", partition="none"))
+    temperature: float = 0.2
+
+    # loading .env
     model_config = {"env_file": ".env", "env_file_encoding": "utf-8"}
 
 
