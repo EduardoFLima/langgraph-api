47f4f08a9e9d7e71b0d9c6242710df2cc1a1d49a
Eduardo Lima <eduefi@gmail.com>
2026-04-13 12:00:52 +0200

    configure agent and invoke it

--- src/adapters/outbound/model_clients/open_api_client.py
+++ src/adapters/outbound/model_clients/open_api_client.py
@@ -21,11 +21,35 @@ class OpenAPIClient(ModelClientPort):
                 "X-Title": self._settings.x_title,
             },
             extra_body={
-                "order":"arcee-ai",
+                "order": "arcee-ai",
                 "models": self._settings.models,
                 "provider": self._settings.provider,
             },
         )
 
     def send_prompt(self, prompt):
-        print("this is the key:", self._settings.open_router_api_key)
+
+        agent = create_agent(model=self._client, tools=[])
+
+        system_prompt = SystemMessage("You are a helpful attendant.")
+        user_prompt = HumanMessage(prompt)
+
+        print("...caling the agent...")
+
+        try:
+            data = agent.invoke({"messages": [system_prompt, user_prompt]})
+
+            received_message = data["messages"][-1]
+            print(
+                "Open Router model used:",
+                received_message.response_metadata["model_name"],
+            )
+
+            answer = received_message.content
+            print("Open Router message:", answer)
+
+        except Exception as e:
+            answer = "An error occurred when calling the llm provider"
+            print(f"{answer}:", e)
+
+        return answer
