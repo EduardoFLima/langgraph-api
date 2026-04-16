960fa045380e51e57a974ba8614758315c9521cb
Eduardo Lima <eduefi@gmail.com>
2026-04-16 22:17:04 +0200

    adjust model client port's send_prompt signature

--- src/domain/ports/model_client_port.py
+++ src/domain/ports/model_client_port.py
@@ -3,5 +3,5 @@ from abc import ABC, abstractmethod
 class ModelClientPort(ABC):
 
     @abstractmethod
-    def send_prompt(self, prompt: str) -> dict:
+    def send_prompt(self, system_prompt: str, user_prompt: str, response_format: type) -> dict:
         raise NotImplementedError("send_prompt is not implemented")
