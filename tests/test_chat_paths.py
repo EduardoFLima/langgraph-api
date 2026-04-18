00bf1616a3e5a5ba42dabbd9e0e6dd15de7cb5cf
Eduardo Lima <eduefi@gmail.com>
2026-04-18 22:39:02 +0200

    organise tests in classes
    for better usability

--- tests/test_chat_paths.py
+++ tests/test_chat_paths.py
@@ -3,33 +3,35 @@ from fastapi.testclient import TestClient
 from src.main import app
 
 
-@pytest.fixture(scope="module")
-def client():
-    return TestClient(app)
+class TestChatPaths:
 
-def test_graph_going_through_path_a(client):
-    client.cookies.set("thread_id", "test_graph_going_through_path_a")
-    response = client.post("/chat", json={"question": "how are u? I just want to go fast, first choice!"})
-    assert response.status_code == 200
-    assert response.json() is not None
+    @pytest.fixture(scope="module")
+    def client(self):
+        return TestClient(app)
 
-    scenario = response.json().get("scenario")
-    assert scenario == "path_a_scenario"
+    def test_graph_going_through_path_a(self, client):
+        client.cookies.set("thread_id", "test_graph_going_through_path_a")
+        response = client.post("/chat", json={"question": "how are u? I just want to go fast, first choice!"})
+        assert response.status_code == 200
+        assert response.json() is not None
 
-def test_graph_going_through_path_b(client):
-    client.cookies.set("thread_id", "test_graph_going_through_path_b")
-    response = client.post("/chat", json={"question": "Im looking for altenratives..."})
-    assert response.status_code == 200
-    assert response.json() is not None
+        scenario = response.json().get("scenario")
+        assert scenario == "path_a_scenario"
 
-    scenario = response.json().get("scenario")
-    assert scenario == "path_b_scenario"
+    def test_graph_going_through_path_b(self, client):
+        client.cookies.set("thread_id", "test_graph_going_through_path_b")
+        response = client.post("/chat", json={"question": "Im looking for altenratives..."})
+        assert response.status_code == 200
+        assert response.json() is not None
 
-def test_graph_going_through_path_unknown(client):
-    client.cookies.set("thread_id", "test_graph_going_through_path_unknown")
-    response = client.post("/chat", json={"question": "I want to order food."})
-    assert response.status_code == 200
-    assert response.json() is not None
+        scenario = response.json().get("scenario")
+        assert scenario == "path_b_scenario"
 
-    scenario = response.json().get("scenario")
-    assert scenario == "unknown_scenario"
+    def test_graph_going_through_path_unknown(self, client):
+        client.cookies.set("thread_id", "test_graph_going_through_path_unknown")
+        response = client.post("/chat", json={"question": "I want to order food."})
+        assert response.status_code == 200
+        assert response.json() is not None
+
+        scenario = response.json().get("scenario")
+        assert scenario == "unknown_scenario"
--- tests/test_chat_persitence.py
+++ tests/test_chat_persitence.py
@@ -3,49 +3,51 @@ from fastapi.testclient import TestClient
 from src.main import app
 
 
-@pytest.fixture(scope="module")
-def client():
-    return TestClient(app)
+class TestChatPersitence:
 
-def test_graph_should_remember_previous_path(client):
-    # first call
-    client.cookies.set("thread_id", "test_graph_should_remember_previous_path")
-    response = client.post("/chat", json={"question": "how are u? I just want to go fast, first choice!"})
-    assert response.status_code == 200
-    assert response.json() is not None
+    @pytest.fixture(scope="module")
+    def client(self):
+        return TestClient(app)
 
-    scenario = response.json().get("scenario")
-    assert scenario == "path_a_scenario"
+    def test_graph_should_remember_previous_path(self, client):
+        # first call
+        client.cookies.set("thread_id", "test_graph_should_remember_previous_path")
+        response = client.post("/chat", json={"question": "how are u? I just want to go fast, first choice!"})
+        assert response.status_code == 200
+        assert response.json() is not None
 
-    # the second call should remember the path
-    client.cookies.set("thread_id", "test_graph_should_remember_previous_path")
-    response = client.post("/chat", json={"question": "how are u? what path did I take?"})
-    assert response.status_code == 200
-    assert response.json() is not None
+        scenario = response.json().get("scenario")
+        assert scenario == "path_a_scenario"
 
-    scenario = response.json().get("scenario")
-    assert scenario == "path_a_scenario"
+        # the second call should remember the path
+        client.cookies.set("thread_id", "test_graph_should_remember_previous_path")
+        response = client.post("/chat", json={"question": "how are u? what path did I take?"})
+        assert response.status_code == 200
+        assert response.json() is not None
 
-def test_graph_should_keep_memory_when_thread_id_is_not_given(client):
-    # first call
-    response = client.post("/chat", json={"question": "how are u? Im skeptical, i want a second path!"})
-    assert response.status_code == 200
+        scenario = response.json().get("scenario")
+        assert scenario == "path_a_scenario"
 
-    assert response.json() is not None
-    scenario = response.json().get("scenario")
-    assert scenario == "path_b_scenario"
+    def test_graph_should_keep_memory_when_thread_id_is_not_given(self, client):
+        # first call
+        response = client.post("/chat", json={"question": "how are u? Im skeptical, i want a second path!"})
+        assert response.status_code == 200
 
-    assert response.cookies is not None
-    thread_id = response.cookies.get("thread_id")
+        assert response.json() is not None
+        scenario = response.json().get("scenario")
+        assert scenario == "path_b_scenario"
 
-    # the second call should remember the path and return the thread id
-    client.cookies.set("thread_id", thread_id)
-    response = client.post("/chat", json={"question": "how are u? what path did I take? "})
-    assert response.status_code == 200
-    assert response.json() is not None
+        assert response.cookies is not None
+        thread_id = response.cookies.get("thread_id")
 
-    scenario = response.json().get("scenario")
-    assert scenario == "path_b_scenario"
+        # the second call should remember the path and return the thread id
+        client.cookies.set("thread_id", thread_id)
+        response = client.post("/chat", json={"question": "how are u? what path did I take? "})
+        assert response.status_code == 200
+        assert response.json() is not None
 
-    assert response.cookies is not None
-    assert response.cookies.get("thread_id") == thread_id
+        scenario = response.json().get("scenario")
+        assert scenario == "path_b_scenario"
+
+        assert response.cookies is not None
+        assert response.cookies.get("thread_id") == thread_id
