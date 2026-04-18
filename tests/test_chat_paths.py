c17d21c4b18aa7ecbb64fa08214e1d6c9e67503a
Eduardo Lima <eduefi@gmail.com>
2026-04-18 22:41:24 +0200

    remove cookies creation in the happy path tests
    as the cookie is now created by the backend service

--- tests/test_chat_paths.py
+++ tests/test_chat_paths.py
@@ -10,27 +10,27 @@ class TestChatPaths:
         return TestClient(app)
 
     def test_graph_going_through_path_a(self, client):
-        client.cookies.set("thread_id", "test_graph_going_through_path_a")
         response = client.post("/chat", json={"question": "how are u? I just want to go fast, first choice!"})
         assert response.status_code == 200
+        assert response.cookies.get("thread_id") is not None
         assert response.json() is not None
 
         scenario = response.json().get("scenario")
         assert scenario == "path_a_scenario"
 
     def test_graph_going_through_path_b(self, client):
-        client.cookies.set("thread_id", "test_graph_going_through_path_b")
         response = client.post("/chat", json={"question": "Im looking for altenratives..."})
         assert response.status_code == 200
+        assert response.cookies.get("thread_id") is not None
         assert response.json() is not None
 
         scenario = response.json().get("scenario")
         assert scenario == "path_b_scenario"
 
     def test_graph_going_through_path_unknown(self, client):
-        client.cookies.set("thread_id", "test_graph_going_through_path_unknown")
         response = client.post("/chat", json={"question": "I want to order food."})
         assert response.status_code == 200
+        assert response.cookies.get("thread_id") is not None
         assert response.json() is not None
 
         scenario = response.json().get("scenario")
