27ec058152bf2235b60694866e040171cdefc0d5
Eduardo Lima <eduefi@gmail.com>
2026-04-18 10:39:23 +0200

    recreate client every test

--- tests/test_chat.py
+++ tests/test_chat.py
@@ -1,9 +1,13 @@
+import pytest
 from fastapi.testclient import TestClient
 from src.main import app
 
-client = TestClient(app)
 
-def test_graph_going_through_path_a():
+@pytest.fixture(scope="module")
+def client():
+    return TestClient(app)
+
+def test_graph_going_through_path_a(client):
     client.cookies.set("thread_id", "test_graph_going_through_path_a")
     response = client.post("/chat", json={"question": "how are u? I just want to go fast, first choice!"})
     assert response.status_code == 200
@@ -12,7 +16,7 @@ def test_graph_going_through_path_a():
     scenario = response.json().get("scenario")
     assert scenario == "path_a_scenario"
 
-def test_graph_going_through_path_b():
+def test_graph_going_through_path_b(client):
     client.cookies.set("thread_id", "test_graph_going_through_path_b")
     response = client.post("/chat", json={"question": "Im looking for altenratives..."})
     assert response.status_code == 200
@@ -21,7 +25,7 @@ def test_graph_going_through_path_b():
     scenario = response.json().get("scenario")
     assert scenario == "path_b_scenario"
 
-def test_graph_going_through_path_unknown():
+def test_graph_going_through_path_unknown(client):
     client.cookies.set("thread_id", "test_graph_going_through_path_unknown")
     response = client.post("/chat", json={"question": "I want to order food."})
     assert response.status_code == 200
@@ -30,7 +34,7 @@ def test_graph_going_through_path_unknown():
     scenario = response.json().get("scenario")
     assert scenario == "unknown_scenario"
 
-def test_graph_should_remember_previous_path():
+def test_graph_should_remember_previous_path(client):
     # first call
     client.cookies.set("thread_id", "test_graph_should_remember_previous_path")
     response = client.post("/chat", json={"question": "how are u? I just want to go fast, first choice!"})
