72c3511cc61226e085009b3d9bf9f4a312f5d290
Eduardo Lima <eduefi@gmail.com>
2026-04-20 22:13:14 +0200

    use store from runtime instead
    making the code cleaner

--- src/application/graph/graph.py
+++ src/application/graph/graph.py
@@ -1,6 +1,5 @@
 from langchain.messages import AIMessage
 from langgraph.graph import END, START, StateGraph
-from langgraph.store.base import BaseStore
 
 from src.application.ports.outbound.memory_port import MemoryPort
 from src.application.ports.outbound.model_client_port import ModelClientPort
@@ -8,46 +7,40 @@ from src.application.graph.graph_state import GraphState, Path
 from src.application.graph.nodes.identify_intent_node import identify_intent
 
 
-def load_memory(store: BaseStore):
-    def load_memory_node(_, runtime):
-        user_id = runtime.context["user_id"] if runtime.context else None
-        memory = store.get(namespace=("preferences", "paths"), key=user_id) if user_id else None
+def load_memory(_, runtime):
+    user_id = runtime.context["user_id"] if runtime.context else None
+    store = runtime.store if runtime.store else None
+    memory = store.get(namespace=("preferences", "paths"), key=user_id) if user_id and store else None
 
-        print("📀 Loaded memory!", memory)
+    print("📀 Loaded memory!", memory)
 
-        if memory is None:
-            return {}
+    if memory is None:
+        return {}
 
-        return {"user_context": {"preferred_path": memory.value["preferred_path"]}}
-
-    return load_memory_node
+    return {"user_context": {"preferred_path": memory.value["preferred_path"]}}
 
 
-def path_a(store: BaseStore):
-    def path_a_node(state: dict, runtime):
-        ai_message = AIMessage("you got here in path_a !")
 
-        store_path(store, state, runtime)
+def path_a(state: dict, runtime):
+    ai_message = AIMessage("you got here in path_a !")
 
-        return {"messages": [ai_message]}
+    store_path(runtime, state)
 
-    return path_a_node
+    return {"messages": [ai_message]}
 
-def path_b(store: BaseStore):
-    def path_b_node(state: dict, runtime):
-        ai_message = AIMessage("you got here in path_b !")
+def path_b(state: dict, runtime):
+    ai_message = AIMessage("you got here in path_b !")
 
-        store_path(store, state, runtime)
+    store_path(runtime, state)
 
-        return {"messages": [ai_message]}
-    return path_b_node
+    return {"messages": [ai_message]}
 
 
-def store_path(store: BaseStore, state, runtime):
+def store_path(runtime, state):
     user_id = runtime.context["user_id"] if runtime.context else None
     path = state["path"]
 
-    store.put(
+    runtime.store.put(
         namespace=("preferences", "paths"),
         key=user_id,
         value={"preferred_path": path.value}
@@ -68,10 +61,10 @@ def path_condition(state: dict):
 def get_graph_definition(model_client: ModelClientPort, memory_saver: MemoryPort):
     agent_builder = StateGraph(GraphState)
 
-    agent_builder.add_node("load_memory", load_memory(memory_saver.get_store()))
+    agent_builder.add_node("load_memory", load_memory)
     agent_builder.add_node("identify_intent", identify_intent(model_client))
-    agent_builder.add_node("path_a", path_a(memory_saver.get_store()))
-    agent_builder.add_node("path_b", path_b(memory_saver.get_store()))
+    agent_builder.add_node("path_a", path_a)
+    agent_builder.add_node("path_b", path_b)
 
     agent_builder.add_edge(START, "load_memory")
     agent_builder.add_edge("load_memory", "identify_intent")
