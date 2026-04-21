f2556dcf47b639423257d6571a82948ecfea378b
Eduardo Lima <eduefi@gmail.com>
2026-04-22 00:04:37 +0200

    add verification whether there is a runtime.context

--- src/application/graph/nodes/load_memory_node.py
+++ src/application/graph/nodes/load_memory_node.py
@@ -1,5 +1,5 @@
 def load_memory(_, runtime):
-    user_id = runtime.context["user_id"] if runtime.context.get("user_id") else None
+    user_id = runtime.context["user_id"] if runtime.context and runtime.context.get("user_id") else None
     store = runtime.store if runtime.store else None
     memory = store.get(namespace=("preferences", "paths"), key=user_id) if user_id and store else None
 
